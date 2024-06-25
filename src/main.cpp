1f5d3d3eba7c375092889231713ff5bb4709de3f
Gallon Zhou <gallonzhou04@gmail.com>
2024-06-25 23:03:28 +1200

    feat: Added Surface Normal Gradient Coloring

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,30 @@
 namespace fs = std::filesystem;
 
 // Check if the ray intersects with the sphere: (source+t*dir)^2
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = center - r.origin();
 
     auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(r.direction(), oc);
+    auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4.0 * a * c;
+    auto t = -1.0;
 
-    return (discriminant >= 0);
+    if (discriminant >= 0) {
+        t = (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+
+    return t;
 }
 
 // Linear interpolate color of ray
 color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -59,7 +68,7 @@ int main() {
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-    fs::path file_path = "snapshots/ray-sphere-intersection-basic.ppm";
+    fs::path file_path = "snapshots/ray-sphere-intersection-gradient.ppm";
     fs::create_directories(file_path.parent_path());
     std::ofstream out_file(file_path);
 
