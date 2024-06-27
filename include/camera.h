46913840a6cc7cdefe5ad133c431734cd8b4bf02
Gallon Zhou <gallonzhou04@gmail.com>
2024-06-27 19:25:31 +1200

    feat: Implemented Defocus Blur or Depth-of-Field

--- include/camera.h
+++ include/camera.h
@@ -16,11 +16,13 @@ class camera {
     point3 lookfrom = point3(0, 0, 0); // Point camera is looking from
     point3 lookat = point3(0, 0, -1);  // Point camera is looking at
     vec3 vup = vec3(0, 1, 0);          // Camera-relative "up" direction
+    double defocus_angle = 0.0;        // Variation angle of rays through each pixel
+    double focus_dist = 10;            // Distance from camera lookfrom point to plane of perfect focus
 
     void render(const hittable &world) {
         initialize();
 
-        fs::path file_path = "snapshots/ray-tracing-camera-20-fov.ppm";
+        fs::path file_path = "snapshots/ray-tracing-defocus-blur.ppm";
         fs::create_directories(file_path.parent_path());
         std::ofstream out_file(file_path);
 
@@ -60,6 +62,8 @@ class camera {
     vec3 pixel_delta_u;         // Offset to pixel to the right
     vec3 pixel_delta_v;         // Offset to pixel below
     vec3 u, v, w;               // Camera coordinate system
+    vec3 defocus_disk_u;        // Defocus disk horizontal radius
+    vec3 defocus_disk_v;        // Defocus disk vertical radius
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -70,10 +74,11 @@ class camera {
         center = lookfrom;
 
         // Define camera parameters
-        auto focal_length = (lookfrom - lookat).length();
+        /* auto focal_length = (lookfrom - lookat).length(); */
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        auto viewport_height = 2 * h * focal_length;
+        /* auto viewport_height = 2 * h * focal_length; */
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
         // Calculate camera's basis vectors
@@ -90,8 +95,13 @@ class camera {
         pixel_delta_v = viewport_v / image_height;
 
         // Get the location of the topleft pixel
-        auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
+        /* auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2; */
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     // Recursively reflect rays and surface color and brightness
@@ -136,7 +146,9 @@ class camera {
         auto offset = sample_square();
         auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
-        auto ray_origin = center;
+        /* auto ray_origin = center; */
+        // Get a ray from the camera through the defocus disk
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
@@ -145,6 +157,13 @@ class camera {
     // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square
     vec3 sample_square() const {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+
+    // Returns a random point in the camera defocus disk.
+    point3 defocus_disk_sample() const {
+        auto p = random_in_unit_disk();
+
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 };
 
--- include/vec3.h
+++ include/vec3.h
@@ -122,6 +122,17 @@ inline vec3 unit_vector(const vec3 &v) {
     return (length > 0) ? v / length : v;
 }
 
+// Generate a random vector within the defocus disk
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+
+        if (p.length_squared() < 1) {
+            return p;
+        }
+    }
+}
+
 // Brute force method of getting a 'valid' random vector
 inline vec3 random_in_unit_sphere() {
     while (true) {
